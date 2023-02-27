03bf57ea9b0c13c7336dafe82294e6739fe99c30
TherryHilaire <therryleseul@gmail.com>
2023-02-26 20:12:42 -0500

    first commit

---
+++ themes/night/theme.bash
@@ -0,0 +1,82 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+#
+# Forest Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#0c0e0f'
+foreground='#edeff0'
+color0='#232526'
+color1='#df5b61'
+color2='#78b892'
+color3='#de8f78'
+color4='#6791c9'
+color5='#bc83e3'
+color6='#67afc1'
+color7='#e4e6e7'
+color8='#2c2e2f'
+color9='#e8646a'
+color10='#81c19b'
+color11='#e79881'
+color12='#709ad2'
+color13='#c58cec'
+color14='#70b8ca'
+color15='#f2f4f5'
+
+accent='#00BCD4'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrains Mono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='adapta.conf'
+geany_font='JetBrains Mono Nerd Font 10'
+
+# Appearance
+gtk_font='Iosevka Nerd Font 9'
+gtk_theme='Adapta-Nokto'
+icon_theme='Archcraft-Dark'
+cursor_theme='Pear'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x40'
+dunst_origin='top-right'
+dunst_font='AestheticIosevka Nerd Font 9'
+dunst_border='2'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='0'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='2'
+bspwm_gap='10'
+bspwm_sratio='0.50'
